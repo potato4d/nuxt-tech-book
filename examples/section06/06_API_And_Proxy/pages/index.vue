e61aec78e10302698026c0506ceddeef84e541a6
HANATANI Takuma <mail@potato4d.me>
2018-03-31 15:52:49 +0900

    Implement API and proxy sample

---
+++ examples/section06/06_API_And_Proxy/pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="container">
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        <p>
+          <img :src="user.profile_image_url" alt="" width="48"><br>
+          <strong>{{user.id}}</strong><br>
+          {{user.description}}
+        </p>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+import AppLogo from '~/components/AppLogo.vue'
+
+export default {
+  async asyncData({ app }) {
+    const users = await app.$axios.$get('/users')
+    return {
+      users
+    }
+  }
+}
+</script>
+
+<style>
+p {
+  margin: 0 0 16px;
+  line-height: 1.5;
+}
+</style>
